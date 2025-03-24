2fb6b19d14b19efd633c55dce7c3b0cdebaa083d
Muhammad Yasir <m.yasir@am.com>
2025-03-24 15:26:25 +0500

    jarvis image added

--- src/app/jarvis/product-view/[id]/page.jsx
+++ src/app/jarvis/product-view/[id]/page.jsx
@@ -4,10 +4,16 @@ import React, { useState, useEffect } from "react";
 
 import RecipeList from "../../../../components/Jarvis/Recipie/RecipeList";
 import { RecipeService } from "../../../../services/recipe.service";
+import Empty_Image from "../../../../../public/images/empty_image.png";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 function Page({ params }) {
   const [recipeData, setRecipeData] = useState([]);
   const [recipeTitleData, setRecipeTitleData] = useState(null);
-
+  console.log("🚀 ~ Page ~ recipeTitleData:", recipeTitleData);
+  const imgUrls = getRecipesImages(recipeTitleData?.image_url);
+  console.log("🚀 ~ Page ~ imgUrls:", imgUrls);
   const [recipeLoading, setRecipeLoading] = useState(true);
   const [error, setError] = useState("");
   useEffect(() => {
@@ -21,7 +27,6 @@ function Page({ params }) {
       setRecipeLoading(true);
       const response = await RecipeService.getRecipes(recipeId);
       const titleResponse = await RecipeService.getRecipesTitle(recipeId);
-      console.log("🚀 ~ getRecipes ~ titleResponse:", titleResponse);
 
       if (titleResponse.items.length) {
         setRecipeTitleData(titleResponse.items[0]);
@@ -43,36 +48,11 @@ function Page({ params }) {
     }
   }
 
-  // if (recipeLoading) {
-  //   return (
-  //     /* From Uiverse.io by ArnavK-09 */ /* From Uiverse.io by clarencedion */
-  //     <div className="flex items-center justify-center min-h-screen bg-[#01040d] pt-3">
-  //       <div className="relative">
-  //         <div className="relative w-32 h-32">
-  //           <div
-  //             className="absolute w-full h-full rounded-full border-[3px] border-gray-100/10 border-r-blue-400 border-b-blue-400 animate-spin"
-  //             style={{ animationDuration: "3s" }}
-  //           />
-  //           <div
-  //             className="absolute w-full h-full rounded-full border-[3px] border-gray-100/10 border-t-blue-400 animate-spin"
-  //             style={{ animationDuration: "2s", animationDirection: "reverse" }}
-  //           />
-  //         </div>
-  //         <div className="absolute inset-0 bg-gradient-to-tr from-blue-400/10 via-transparent to-blue-400/5 animate-pulse rounded-full blur-sm" />
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-  // if (error) {
-  //   return (
-  //     <>
-  //       <div className="h-[100vh] items-center justify-center flex bg-[#01040d]">
-  //         <h1 className="text-4xl text-white">{error}</h1>
-  //       </div>
-  //     </>
-  //   );
-  // }
+  function getRecipesImages(imgUrls) {
+    if (!imgUrls) return;
+    const urls = imgUrls.split(", ");
+    return urls;
+  }
 
   return (
     <>
@@ -123,18 +103,35 @@ to-[#334155] "
         ) : (
           <>
             <div className="mt-10">
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center ">
                 <h1 className="text-white text-center font-extrabold tracking-wide sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
                   {recipeTitleData?.fabric ?? "N/A"}
                 </h1>
 
-                <div className="relative mt-6 rounded-xl overflow-hidden shadow-lg ring-2 ring-gray-500 sm:mt-8 w-[90%] xs:w-[90%] sm:w-[90%] md:w-[85%] lg:max-w-[30%] aspect-video">
+                {imgUrls && imgUrls.length > 0 ? (
+                  <div className="  overflow-hidden relative mt-6 rounded-xl shadow-lg ring-2 ring-gray-500 sm:mt-8 w-[90%] xs:w-[90%] sm:w-[90%] md:w-[85%] lg:max-w-[30%]  flex flex-col justify-center items-center ">
+                    <Carousel
+                      showStatus={false}
+                      showThumbs={false}
+                      className=" w-full  h-[300px]"
+                    >
+                      {imgUrls.map((img, index) => (
+                        <img
+                          key={index}
+                          src={img}
+                          alt="recipe img"
+                          className="object-contain  h-[300px] "
+                        />
+                      ))}
+                    </Carousel>
+                  </div>
+                ) : (
                   <img
-                    className="object-cover w-full h-full transition-transform duration-300 hover:scale-105"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL3ZOIs495C8D7dMKaLMxQtS4VTJ6G0XM4uw&s"
-                    alt="machine"
+                    src={Empty_Image.src}
+                    alt="no image"
+                    className="object-contain h-[300px]"
                   />
-                </div>
+                )}
               </div>
 
               {/* <div className="flex gap-4">
