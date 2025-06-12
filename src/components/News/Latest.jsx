548107c8c63bb38d67de80807112bb4e80bafb76
Muhammad-Usama-lab <osamawaseem294@gmail.com>
2025-06-12 17:54:44 +0500

    news page completed.

--- src/components/News/Latest.jsx
+++ src/components/News/Latest.jsx
@@ -15,8 +15,8 @@ const Latest = () => {
       <div className="grid h-full grid-cols-1 lg:grid-cols-2 pt-8">
         <div className="flex flex-col justify-between border-black px-8 py-0">
           <h1 className={`max-w-xl font-bold text-lg md:text-2xl`}>
-            AM Mexico, Artistic Milliners’ Newest Western Hemisphere Facility,
-            Begins Production
+          AM Mexico, Artistic Milliners' Newest Western Hemisphere Facility,
+          Begins Production
           </h1>
           <h5 className="max-w-3xl mt-8 text-sm sm:text-xl lg:text-2xl grow sm:mb-14">
             LOS ANGELES | June 12, 2025 | Artistic Milliners, a global leader in
@@ -34,30 +34,7 @@ const Latest = () => {
               <h6 className="text-sm sm:text-xl uppercase">news</h6>
               <h4 className="text-xl sm:text-5xl">24.02.25</h4>
             </div>
-            <a
-              href="/Yess-Certificate-AM.pdf"
-              download
-              className={`flex items-center max-w-40 sm:max-w-52 gap-x-1 uppercase border-2 border-black hover:bg-gray-100 dark:border-gray-300 rounded-full px-4 sm:px-10 py-1 sm:py-3 text-sm font-semibold`}
-            >
-              Learn more
-              <div>
-                {
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.3em"
-                    height="1.3em"
-                    viewBox="0 0 15 15"
-                  >
-                    <path
-                      fill="currentColor"
-                      fill-rule="evenodd"
-                      d="M7.5 1.05a.45.45 0 0 1 .45.45v6.914l2.232-2.232a.45.45 0 1 1 .636.636l-3 3a.45.45 0 0 1-.636 0l-3-3a.45.45 0 1 1 .636-.636L7.05 8.414V1.5a.45.45 0 0 1 .45-.45M2.5 10a.5.5 0 0 1 .5.5V12c0 .554.446 1 .996 1h7.005A1 1 0 0 0 12 12v-1.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-1.999 2H3.996A1.997 1.997 0 0 1 2 12v-1.5a.5.5 0 0 1 .5-.5"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                }
-              </div>
-            </a>
+            <Button label="Learn More" goTo="/news/1" />
           </div>
         </div>
         <div className="flex flex-col justify-between border-black p-8"></div>
--- src/components/News/LatestNews.jsx
+++ src/components/News/LatestNews.jsx
@@ -20,93 +20,56 @@ const LatestNews = () => {
         <div className="grid h-full grid-cols-1 lg:grid-cols-2">
           <div className="flex flex-col justify-between border-black border-t lg:border  p-8">
             <h1 className={`max-w-xl font-bold text-lg md:text-2xl`}>
-              Artistic Milliners Takes Major Step Towards YESS Certification
+              Sowing Seeds of Change: Empowering Women and Transforming Cotton
+              Farming
             </h1>
             <h5 className="max-w-3xl mt-14 text-sm sm:text-xl lg:text-2xl grow sm:mb-14">
-              KARACHI, PAKISTAN – February 24, 2025 – Artistic Millinersis
-              pleased to announce that it has been officially listed on the Yarn
-              Ethically Sustainably Sourced (YESS) Assessment Portal. This
-              significant milestone underscores the company's unwavering
-              commitment to ethical and sustainable sourcing practices
-              throughout its supply chain. The YESS certification is a globally
-              recognized standard that ensures the responsible procurement of
-              cotton and cotton-based products. By fulfilling all the necessary
-              requirements, Artistic Milliners is on track to achieve full YESS
-              certification, further solidifying its position as a responsible
-              and sustainable industry leader.
+              Artistic Milliners, in partnership with the Centre of Excellence
+              in Responsible Business (CERB) and the Pakistan Business Council,
+              is driving significant change in rural Pakistan through the
+              Milliner Cotton Initiative (MCI). This program is dedicated to
+              empowering rural women and transforming cotton farming through
+              sustainable practices.
+              <br />
+              The MCI addresses systemic barriers faced by women in rural areas
+              by introducing two key programs: the Alternate Livelihood Program
+              (ALP) and Fields of Equity.
             </h5>
-            {/* <h5 className="my-4 text-sm sm:text-lg grow ">
-                "We are thrilled to be listed on the YESS Assessment Portal and
-                are diligently working towards obtaining the YESS
-                certification," said Akbar Ali, Manager, Responsible Business
-                Projects at Artistic Milliners. "This accomplishment reflects
-                our dedication to responsible sourcing and our commitment to
-                creating a positive impact on the communities we operate in."
-              </h5>
-              <h5 className="text-sm sm:text-lg grow mb-5">
-                Artistic Milliners' commitment to YESS aligns with its broader
-                sustainability strategy, which includes reducing its
-                environmental footprint, promoting fair labor practices, and
-                ensuring ethical sourcing throughout its supply chain. The
-                company's YESS policy outlines its expectations for suppliers,
-                including the avoidance of forced labor, fair working
-                conditions, and adherence to industry standards.
-              </h5> */}
 
             <div className="flex justify-between items-center mt-6 sm:mt-0">
               <div>
                 <h6 className="text-sm sm:text-xl uppercase">news</h6>
                 <h4 className="text-xl sm:text-5xl">24.02.25</h4>
               </div>
-              <a
-                href="/Yess-Certificate-AM.pdf"
-                download
-                className={`flex items-center max-w-40 sm:max-w-52 gap-x-1 uppercase border-2 border-black hover:bg-gray-100 dark:border-gray-300 rounded-full px-4 sm:px-10 py-1 sm:py-3 text-sm font-semibold`}
-              >
-                download
-                <div>
-                  {
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="1.3em"
-                      height="1.3em"
-                      viewBox="0 0 15 15"
-                    >
-                      <path
-                        fill="currentColor"
-                        fill-rule="evenodd"
-                        d="M7.5 1.05a.45.45 0 0 1 .45.45v6.914l2.232-2.232a.45.45 0 1 1 .636.636l-3 3a.45.45 0 0 1-.636 0l-3-3a.45.45 0 1 1 .636-.636L7.05 8.414V1.5a.45.45 0 0 1 .45-.45M2.5 10a.5.5 0 0 1 .5.5V12c0 .554.446 1 .996 1h7.005A1 1 0 0 0 12 12v-1.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-1.999 2H3.996A1.997 1.997 0 0 1 2 12v-1.5a.5.5 0 0 1 .5-.5"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  }
-                </div>
-              </a>
+              <Button label="Learn More" goTo="/news/2" />
             </div>
           </div>
           <div className="flex flex-col justify-between border-black border-t border-b lg:border p-8">
-            <h1 className={`max-w-2xl font-bold text-lg md:text-2xl`}>
-              Proud Partner with WWF - Pakistan on Local Water Stewardship
+            <h1 className={`max-w-2xl font-bold text-lg`}>
+              Artistic Milliners Joins Forces with SFI and The LYCRA Company to
+              Revolutionize Denim with LYCRA FitSense® Technology
             </h1>
             <h5 className="max-w-3xl mt-14 text-sm sm:text-xl lg:text-2xl grow sm:mb-14">
-              We are proud to partner with WWF - Pakistan to promote Water
-              Stewardship among the vulnerable communities of Karachi. We
-              recognize the critical importance of preserving and managing our
-              freshwater resources. Our partnership with WWF - Pakistan is
-              focused on implementing strategic water stewardship practices that
-              not only benefit our operations but also support the broader
-              community and ecosystem. We engaged with various stakeholders,
-              including neighboring companies and local communities, to share
-              insights and collaborate on innovative water management
-              strategies. Our goal is to foster a collective approach to address
-              water-related challenges and promote best practices.
+              KARACHI, PAKISTAN – APRIL 10, 2025 – Artistic Milliners is proud
+              to announce its pivotal role in an exciting new partnership
+              bringing LYCRA FitSense® denim technology to the Western
+              Hemisphere. This collaboration, alongside Star Fades International
+              (SFI), a leader in premium denim innovation, and The LYCRA
+              Company, a global pioneer in fiber and fabric technology, marks a
+              significant step forward in providing innovative and
+              high-performance denim solutions.
+              <br />
+              As a key partner, Artistic Milliners Mexico will be instrumental
+              in bringing this patented technology closer to brands and
+              consumers, enabling faster production, greater flexibility, and
+              improved supply chain agility in the region.
             </h5>
             <div className="flex justify-between items-center mt-6 sm:mt-0">
               <div>
                 <h6 className="text-sm sm:text-xl uppercase">news</h6>
                 <h4 className="text-xl sm:text-5xl">13.11.24</h4>
               </div>
-              <Button label="Learn More" />
+              <Button label="Learn More" goTo="/news/3" />
             </div>
           </div>
         </div>
